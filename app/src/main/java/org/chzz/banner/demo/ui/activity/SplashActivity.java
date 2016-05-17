11e486a9a6de9ed2cf2b78c79993d019fa18042c
copy <xiaoxinxing12@qq.com>
2016-05-17 13:49:01 +0800

    Initial commit

---
+++ app/src/main/java/org/chzz/banner/demo/ui/activity/SplashActivity.java
@@ -0,0 +1,54 @@
+package org.chzz.banner.demo.ui.activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.DrawableRes;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageView;
+
+import org.chzz.banner.CHZZBanner;
+import org.chzz.banner.demo.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        CHZZBanner banner = (CHZZBanner)findViewById(R.id.banner_splash_pager);
+        // 用Java代码方式设置切换动画
+        banner.setTransitionEffect(CHZZBanner.TransitionEffect.Rotate);
+        // banner.setPageTransformer(new RotatePageTransformer());
+        // 设置page切换时长
+        banner.setPageChangeDuration(1000);
+        List<View> views = new ArrayList<>();
+        views.add(getPageView(R.drawable.guide_1));
+        views.add(getPageView(R.drawable.guide_2));
+        views.add(getPageView(R.drawable.guide_3));
+
+        View lastView = getLayoutInflater().inflate(R.layout.view_last, null);
+        views.add(lastView);
+        lastView.findViewById(R.id.btn_last_main).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        });
+        banner.setViews(views);
+        // banner.setCurrentItem(1);
+    }
+
+    private View getPageView(@DrawableRes int resid) {
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(resid);
+        imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+        return imageView;
+    }
+}
